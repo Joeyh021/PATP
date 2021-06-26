ce7b910259890386928545471aabd030f09bf1d6
Joe Harrison <josephharrison1@me.com>
2021-06-27 00:42:02 +0100

    parse_file now takes an &str

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,17 @@ mod parser;
 
 fn main() {
     //get the args from the command line as a vector of Strings
+
     let args: Vec<String> = args().collect();
     if args.len() < 2 {
         panic!("please enter a filename")
     }
-    let filename = args[1];
 
-    let lines = fs::read_to_string(filename)
-        .expect("Could not open file!")
-        .lines()
-        .collect();
+    let filename: &str = &args[1];
 
-    let binary: Vec<u8> = parser::parse_file(lines);
+    let file: String = fs::read_to_string(filename).expect("Could not open file!");
+
+    let binary: Vec<u8> = parser::parse_file(&file);
 
     fs::write("output", &binary).expect("Error writing to file");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,23 +1,24 @@
 use std::collections::HashMap;
 
+use crate::instruction::Instruction;
+
 //take a line, parse the relevant symbols/words, return the instruction it represents
 //instructions are newline-seperated, and of format
 // label: opcode operand ; comment
-use crate::instruction::Instruction;
-
-fn parseline(table: &mut HashMap<&str, u8>, line: &str, lineno: usize) -> Option<Instruction> {
+// returns Some(Instruction), or None if there's some parse error
+fn parse_line(line: &str, lineno: usize, table: &mut HashMap<&str, u8>) -> Option<Instruction> {
     //copy the instruction so we have ownership of it
     let mut line: String = String::from(line);
 
     let mut label: Option<&str> = None;
     let mut operand: Option<u8> = None;
 
-    //remove the comment from the line
+    //remove the comment from the line, if one exists
     if let Some(i) = line.find(';') {
         line.truncate(i);
     }
 
-    //store the label
+    //get the label, if the line has one
     if let Some(i) = line.find(':') {
         label = Some(&line[0..i]);
     }
@@ -39,22 +40,24 @@ fn parseline(table: &mut HashMap<&str, u8>, line: &str, lineno: usize) -> Option
         "BUZ" | "BNZ" | "BZC" | "BNE" => Instruction::BNZ(operand?),
         "LOAD" => Instruction::LOAD(operand?),
         "STORE" => Instruction::STORE(operand?),
+        _ => return None,
     };
+
     return Some(instruction);
 }
 
 //the main assembler function
 //takes a vec of strings (the file) and returns a vec of instructions
-pub fn parse_file(lines: Vec<&str>) -> Vec<u8> {
+pub fn parse_file(file: &str) -> Vec<u8> {
     //keeps track of symbols and their names/locations
     let mut symbol_table: HashMap<&str, u8> = HashMap::new();
     let mut binary: Vec<u8> = Vec::new();
 
-    let mut instructions = lines.iter().enumerate();
+    let mut instructions = file.lines().enumerate();
 
     while let Some((lineno, line)) = instructions.next() {
         binary.push(
-            parseline(&mut symbol_table, line, lineno)
+            parse_line(&line, lineno, &mut symbol_table)
                 .unwrap()
                 .assemble(),
         );
