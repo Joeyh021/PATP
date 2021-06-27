4bc52b04962ffa5abfefe82d7e80abbc93d37886
Joe Harrison <josephharrison1@me.com>
2021-06-27 17:21:54 +0100

    added a few file tests

--- src/parser/test_files.rs
+++ src/parser/test_files.rs
@@ -1,3 +1,18 @@
 use super::*;
 
-fn parse_file_empty() {}
+#[test]
+fn parse_file_empty() {
+    assert_eq!(parse_file(""), Vec::new());
+    assert_eq!(parse_file("         "), Vec::new());
+    assert_eq!(parse_file("       \n  "), Vec::new());
+    assert_eq!(parse_file("       \n  ;   \n"), Vec::new());
+}
+
+#[test]
+fn parse_file_one() {
+    assert_eq!(parse_file("\n CLEAR \n"), vec![0]);
+    assert_eq!(
+        parse_file("CLEAR \n ADD 15 \n STORE 0"),
+        vec![0, 0b010_01111, 0b111_00000]
+    );
+}
