25d1d58612c7c6b770ad46791267877b4be7381a
Joe Harrison <josephharrison1@me.com>
2022-05-13 23:16:32 +0100

    updated to latest clap

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 use anyhow::Result;
+use clap::{ArgEnum, Parser};
 use std::{fs, path::Path};
 
 mod assembler;
-mod cli;
 mod emulator;
 mod parser;
 
 fn main() -> Result<()> {
-    let matches = cli::args().get_matches();
+    let args = Cli::parse();
+    let file: &Path = args.file.as_ref();
 
-    if matches.is_present("emulate") {
-        emulator::execute_file(Path::new(matches.value_of("emulate").unwrap()))?
-    } else if matches.is_present("assemble") {
-        assembler::assemble_file(Path::new(matches.value_of("assemble").unwrap()))?
-    } else if matches.is_present("run") {
-        run_file(Path::new(matches.value_of("run").unwrap()))?
-    } else {
-        //shouldn't happen if cli parser does its job
-        //unwraps should never error either
-        panic!()
+    match args.command {
+        Command::Assemble => emulator::execute_file(file)?,
+        Command::Emulate => assembler::assemble_file(Path::new(file))?,
+        Command::Run => run_file(Path::new(file))?,
     }
     Ok(())
 }
@@ -33,4 +28,30 @@ pub fn run_file(path: &Path) -> Result<()> {
     println!("Final CPU State: \n{}", final_state);
 
     Ok(())
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(arg_enum)]
+    command: Command,
+    #[clap(validator = file_exists)]
+    file: String,
+}
+
+#[derive(Copy, Clone, Debug, ArgEnum)]
+enum Command {
+    Assemble,
+    Emulate,
+    Run,
+}
+
+fn file_exists(f: &str) -> Result<(), &'static str> {
+    let p = std::path::Path::new(f);
+    if !p.is_file() {
+        Err("File does not exist.")
+    } else {
+        Ok(())
+    }
 }
